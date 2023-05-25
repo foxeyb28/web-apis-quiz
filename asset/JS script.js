b33837c93e298be12a97a0ce0c4eb5f9307c41c4
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-05-24 21:54:43 -0500

    adding html starter code

---
+++ asset/JS script.js
@@ -0,0 +1,22 @@
+//WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+/*
+// every question will be a:
+{
+    question: "what does Css stand for?";
+    option1: "option here";
+    option2: "another example here";
+    option3: "this one too";
+    correctanswer: "this is right answer";
+}
+all questions will be stored in an array, which will look like this:
+pq1, q2, 1=q3
+*/
