3fb42787c29b61c076ef754f12bb16289da8baa9
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-05-29 23:33:21 -0500

    added questions for quiz

--- asset/script.js
+++ asset/script.js
@@ -20,4 +20,78 @@
 all questions will be stored in an array, which will look like this:
 pq1, q2, 1=q3
 */
+
+var questionsDiv = document.querySelector("#Code Quiz Challenge");
+
+function renderQuestion(questionObj) {
+    var question = document.createElement("h1");
+    // get question from obj
+    question.textContent = "what is needed to make a 3 ingrediant cake";
+    questionsDiv.appendChild (question)
+
+    var options = document.createElement("ol");
+    questionsDiv.appendChild(options);
+
+    var option1 =document.createElement("li")
+    // get one option from obj
+    option1.textContent = "Peach Pie Filling";
+    options.appendChild(option1);
+
+    var option2 = document.createElement("li");
+    // get one option from obj
+    option2.textContent = "5 Eggs";
+    options.appendChild(option2);
+
+    var option3 = document.createElement("li")
+    // get one option from obj
+    option3.textContent = "Cake Mix"
+    options.appendChild(option3);
+
+    var option4 = document.createElement("li");
+    // get one option from obj
+    option4.textContent = "A Bundt Pan"
+    options.appendChild(option4);
+}
+
+// object with 3 properties: question, list of options, correct answer
+
+var obj = {
+    "question": "Which Pie Filling is needed to make a Peach 3 ingrediant cake", 
+    "options": ["Peach Pie Filling", "Apple Pie Filling", "Cherry Pie Filling", "Blueberry"], 
+    "answer": "Peach Pie Filling"
+}
+
+renderQuestion(obj);
+
+var obj = {
+    "question": "How many eggs are needed for the cake", 
+    "options": ["1", "2", "3", "4", "5"], 
+    "answer": "5"
+}
+
+renderQuestion(obj);
+
+var obj = {
+    "question": "Is cake mix needed", 
+    "options": ["Yes", "No"], 
+    "answer": "Yes"
+}
+
+renderQuestion(obj);
+var obj = {
+    "question": " what is the cake cooked in", 
+    "options": ["A Plate", "A Bowl", "A Skillet", "A Bundt Pan"], 
+    "answer": "Peach Pie Filling"
+}
+
+renderQuestion(obj);
+
+var obj = {
+    "question": "Is Peach Pie Filling needed to make a Peach 3 ingrediant cake", 
+    "options": ["Peach Pie Filling", "Apple Pie Filling", "Cherry Pie Filling", "Blueberry"], 
+    "answer": "Peach Pie Filling"
+}
+
+renderQuestion(obj);
+
 var endTime
